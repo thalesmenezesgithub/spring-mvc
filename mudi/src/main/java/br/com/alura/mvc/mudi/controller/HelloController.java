057411f78b5a2a304f90d92f0f2aa139efca7bd2
Thales R. Menezes <99663601+thalesmenezesgithub@users.noreply.github.com>
2023-03-06 16:28:27 -0300

    Início Spring MVC
    Configuração inicial do Spring MVC

---
+++ mudi/src/main/java/br/com/alura/mvc/mudi/controller/HelloController.java
@@ -0,0 +1,17 @@
+package br.com.alura.mvc.mudi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class HelloController
+{
+    @GetMapping("/hello")
+    public String hello(HttpServletRequest request)
+    {
+        request.setAttribute("nome","mundo");
+        return "hello";
+    }
+}
